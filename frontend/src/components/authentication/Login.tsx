c750b3f6434e6599f5f29c9899f7a3ff6a429c5d
Hairum Qureshi <hqureshi@udel.edu>
2024-06-07 23:49:02 -0400

    styled both the Login and Registration components and added buttons to each

--- frontend/src/components/authentication/Login.tsx
+++ frontend/src/components/authentication/Login.tsx
@@ -1,12 +1,20 @@
+// TODO - add a link redirecting the user to the register page if they don't have an account
+
+import { Link } from "react-router-dom";
+
 export default function Login() {
 	return (
-		<div className="bg-white">
+		<div className="bg-white p-5">
 			<h1 className="text-5xl font-semibold text-center lg:text-left">
 				Welcome back!
 			</h1>
 			<p className="font-medium text-lg text-gray-500 mt-4 text-center lg:text-left">
-				Welcome back to React Chat, please enter your details to login and
-				resume chatting.
+				Welcome back to React Chat. Please enter your details to login and
+				resume chatting. Don't have an account? Click&nbsp;
+				<Link to="/sign-up" className="text-blue-600">
+					here
+				</Link>{" "}
+				to create an account!
 			</p>
 			<div className="mt-8">
 				<div>
@@ -47,6 +55,11 @@ export default function Login() {
 						Forgot Password
 					</button>
 				</div>
+			</div>
+			<div className="w-full mt-6 text-center">
+				<button className="text-xl m-auto border-2 py-2.5 rounded-md w-1/2 bg-blue-600 text-white">
+					Login
+				</button>
 			</div>
 		</div>
 	);
--- frontend/src/components/authentication/Registration.tsx
+++ frontend/src/components/authentication/Registration.tsx
@@ -1,32 +1,78 @@
-import axios from "axios";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Registration() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
 
-	async function registerUser(e: React.FormEvent) {
-		e.preventDefault();
-		if (username && email && password) {
-			try {
-				const response = await axios.post(
-					"http://localhost:3000/api/auth/register",
-					{
-						username,
-						email,
-						password
-					},
-					{
-						withCredentials: true
-					}
-				);
-				console.log(response);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-	}
-
-	return <div>Sign up</div>;
+	return (
+		<div className="bg-white p-5">
+			<h1 className="text-5xl font-semibold text-center lg:text-left">
+				Welcome!
+			</h1>
+			<p className="font-medium text-lg text-gray-500 mt-4 text-center lg:text-left">
+				Welcome to React Chat and thank you for creating an account. We hope
+				you'll enjoy chatting with your friends!
+			</p>
+			<div className="mt-8">
+				<div>
+					<label htmlFor="email" className="text-lg font-medium ml-2 lg:ml-0">
+						Username
+					</label>
+					<input
+						type="text"
+						id="username"
+						placeholder="Username"
+						className="w-full border-2 border-gray-100 rounded-md mt-1 p-4 outline-gray-300"
+					/>
+				</div>
+			</div>
+			<div>
+				<div className="mt-3">
+					<label htmlFor="email" className="text-lg font-medium ml-2 lg:ml-0">
+						Email
+					</label>
+					<input
+						type="email"
+						id="email"
+						placeholder="Email"
+						className="w-full border-2 border-gray-100 rounded-md mt-1 p-4 outline-gray-300"
+					/>
+				</div>
+			</div>
+			<div>
+				<div className="mt-3">
+					<label
+						htmlFor="password"
+						className="text-lg font-medium ml-2 lg:ml-0"
+					>
+						Password
+					</label>
+					<input
+						type="password"
+						id="password"
+						placeholder="Password"
+						className="w-full border-2 border-gray-100 rounded-md mt-1 p-4 outline-gray-300"
+					/>
+				</div>
+			</div>
+			<div>
+				<div className="flex">
+					<input type="checkbox" id="remember-login" className="ml-3 lg:ml-0" />
+					<label htmlFor="remember-login" className="ml-2">
+						Remember Me
+					</label>
+					<button
+						className="ml-auto text-purple-700 font-semibold mr-3 lg:mr-0"
+						onClick={() => navigate("/sign-in")}
+					>
+						Already have an account?
+					</button>
+				</div>
+			</div>
+			<div className="w-full mt-6 text-center">
+				<button className="text-xl m-auto border-2 py-2.5 rounded-md w-1/2 bg-blue-600 text-white">
+					Create Account
+				</button>
+			</div>
+		</div>
+	);
 }
