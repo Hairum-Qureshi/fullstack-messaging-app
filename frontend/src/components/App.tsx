8df0bfdb33a928416a83f1c25160b644f277e26f
Hairum Qureshi <hqureshi@udel.edu>
2024-06-10 12:23:40 -0400

    created custom authentication context and wrapped it around components in App.tsx

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,17 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import Authentication from "./authentication/Authentication";
+import Inbox from "./Inbox";
+import { AuthProvider } from "../contexts/authContext";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			{/* <Navbar /> */}
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/sign-up" element={<Authentication />} />
-				<Route path="/sign-in" element={<Authentication />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			<AuthProvider>
+				{/* <Navbar /> */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/sign-up" element={<Authentication />} />
+					<Route path="/sign-in" element={<Authentication />} />
+					<Route path="/messages" element={<Inbox />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</AuthProvider>
 		</BrowserRouter>
 	);
 }
---
+++ frontend/src/contexts/authContext.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect, createContext, useContext } from "react";
+import axios from "axios";
+import { User, ContextData, AuthProps } from "../interfaces";
+
+export const AuthContext = createContext<ContextData | null>(null);
+
+export const AuthProvider = ({ children }: AuthProps) => {
+	const [userData, setUserData] = useState<User | null>(null);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const getCurrUserData = async () => {
+			try {
+				const userDataResponse = await axios.get(
+					"http://localhost:3000/api/user/current",
+					{
+						withCredentials: true
+					}
+				);
+				if (userDataResponse.data.message === "user is not logged in") {
+					setUserData(null);
+				} else {
+					setUserData(userDataResponse.data);
+				}
+			} catch (error) {
+				console.error("There was an error", error);
+			}
+		};
+
+		getCurrUserData();
+	}, []);
+
+	const signOut = async () => {
+		try {
+			const response = await axios.get(
+				"http://localhost:3000/api/auth/logout",
+				{
+					withCredentials: true
+				}
+			);
+			if (response.status === 200) {
+				setUserData(null);
+			}
+		} catch (error) {
+			console.error("There was an error", error);
+		}
+	};
+
+	const value: ContextData = {
+		userData,
+		error,
+		signOut
+	};
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuthContext = () => {
+	return useContext(AuthContext);
+};
+
+export default useAuthContext;
