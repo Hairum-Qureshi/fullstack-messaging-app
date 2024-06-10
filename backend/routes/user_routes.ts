911d5be32c969efa01ff7241921ecb3c3b86ccfc
Hairum Qureshi <hqureshi@udel.edu>
2024-06-10 12:22:49 -0400

    implemented logic to decode current user session cookie to retrieve current user data

--- backend/routes/user_routes.ts
+++ backend/routes/user_routes.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import { Request, Response } from "express";
-import { StreamChat } from "stream-chat";
 import dotenv from "dotenv";
-
-dotenv.config();
+import { streamChat } from "./auth_routes";
 
 const router = express.Router();
 
-router.get("/current", (req: Request, res: Response) => {
-	console.log(req.cookies);
-	console.log(
-		JSON.parse(
-			Buffer.from(
-				req.cookies["auth-session"].split(".")[1],
-				"base64"
-			).toString()
-		)
-	);
-	res.send("Current user route");
+router.get("/current", async (req: Request, res: Response) => {
+	if (req.cookies !== undefined) {
+		const token = req.cookies["auth-session"];
+		const payloadBase64 = token.split(".")[1];
+		const payloadBuffer = Buffer.from(payloadBase64, "base64");
+		const payload = JSON.parse(payloadBuffer.toString());
+		const current_uid: string = payload.user_id;
+		const current_userData = await streamChat.queryUsers({ id: current_uid });
+		const { id, username, email } = current_userData.users[0];
+		res.json({ uid: id, username, email });
+	} else {
+		res.json({ message: "user not logged in" });
+	}
 });
 
 export default router;
