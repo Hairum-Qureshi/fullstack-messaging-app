09f35e5e408d298a8275af304f2681db15b7ba8e
Hairum Qureshi <hqureshi@udel.edu>
2024-06-17 11:05:44 -0400

    removed Stream components and logic

--- frontend/src/components/Inbox.tsx
+++ frontend/src/components/Inbox.tsx
@@ -4,7 +4,12 @@ import Home from "./Home";
 export default function Inbox() {
 	const { userData } = useAuthContext()!;
 
-	console.log(userData);
-
-	return userData?.message !== "user not logged in" ? <h1>Inbox</h1> : <Home />;
+	return userData?.message !== "user not logged in" ? (
+		<div className="flex w-full box-border">
+			<div className="w-full lg:w-1/4 h-screen bg-slate-200"></div>
+			<div className="w-full border-2 hidden border-blue-600 lg:block h-screen"></div>
+		</div>
+	) : (
+		<Home />
+	);
 }
--- frontend/src/contexts/authContext.tsx
+++ frontend/src/contexts/authContext.tsx
@@ -1,15 +1,12 @@
 import { useState, useEffect, createContext, useContext } from "react";
 import axios from "axios";
 import { User, ContextData, AuthProps } from "../interfaces";
-import { StreamChat } from "stream-chat";
 
 export const AuthContext = createContext<ContextData | null>(null);
 
 export const AuthProvider = ({ children }: AuthProps) => {
 	const [userData, setUserData] = useState<User | null>(null);
 	const [error, setError] = useState<string | null>(null);
-	const [token, setToken] = useState<string | null>(null);
-	const [streamChat, setStreamChat] = useState<StreamChat>();
 
 	useEffect(() => {
 		const getCurrUserData = async () => {
@@ -24,7 +21,6 @@ export const AuthProvider = ({ children }: AuthProps) => {
 					setUserData(null);
 				} else {
 					setUserData(userDataResponse.data);
-					setToken(userDataResponse.data.token);
 				}
 			} catch (error) {
 				console.error("There was an error", error);
@@ -50,43 +46,10 @@ export const AuthProvider = ({ children }: AuthProps) => {
 		}
 	};
 
-	useEffect(() => {
-		if (!userData) {
-			return;
-		}
-		const chat = new StreamChat(import.meta.env.VITE_STREAM_API_KEY);
-
-		if (chat.tokenManager.token === token && chat.userID === userData.id)
-			return;
-
-		const userResponse = {
-			id: userData.id,
-			username: userData.username,
-			email: userData.email,
-			profile_picture: userData.profile_picture
-		};
-
-		let isInterrupted = false;
-		const connectPromise = chat.connectUser(userResponse, token).then(() => {
-			if (isInterrupted) return;
-			setStreamChat(chat);
-		});
-
-		return () => {
-			isInterrupted = true;
-			setStreamChat(undefined);
-
-			connectPromise.then(() => {
-				chat.disconnectUser();
-			});
-		};
-	}, [token, userData]);
-
 	const value: ContextData = {
 		userData,
 		error,
-		signOut,
-		streamChat
+		signOut
 	};
 
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
