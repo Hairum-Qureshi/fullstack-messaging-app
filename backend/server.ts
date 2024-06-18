9fc9add3c81af3531052f1629e90e8f87ae446a0
Hairum Qureshi <hqureshi@udel.edu>
2024-06-18 15:31:29 -0400

    created connection with socket.io with frontend and with MongoDB

--- backend/server.ts
+++ backend/server.ts
@@ -4,11 +4,14 @@ import auth_routes from "./routes/auth_routes";
 import user_routes from "./routes/user_routes";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
-const PORT: string = process.env.PORT!;
+const PORT: string = process.env.PORT! || "3000";
+const MONGO_URI: string = process.env.MONGO_URI!;
 
 const corsOptions = {
 	origin: "http://localhost:5174",
@@ -24,6 +27,25 @@ app.use(cookieParser());
 app.use("/api/auth", auth_routes);
 app.use("/api/user", user_routes);
 
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}!`);
-});
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		const express_server = app.listen(PORT, () => {
+			console.log(
+				`Successfully connected to MongoDB! Server listening on port ${PORT}`
+			);
+		});
+
+		const io = new Server(express_server, {
+			cors: {
+				origin: ["http://localhost:5174"]
+			}
+		});
+
+		io.on("connection", socket => {
+			console.log(`User with socket ID: ${socket.id} connected`);
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});
