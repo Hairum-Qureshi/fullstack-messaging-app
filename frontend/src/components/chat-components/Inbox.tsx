69b941527d6b6b6f3c7b7986266e4327438529de
Hairum Qureshi <hqureshi@udel.edu>
2024-06-19 00:22:17 -0400

    updated logic to render out the chat layout or Home component depending on authenticated status, added logic to display the current user's username next to the hamburger menu as well

--- frontend/src/components/chat-components/Inbox.tsx
+++ frontend/src/components/chat-components/Inbox.tsx
@@ -4,23 +4,36 @@ import Home from "../Home";
 import ContactBlock from "./ContactBlock";
 import Conversation from "./Conversation";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import useMessaging from "../../hooks/useMessaging";
 
 export default function Inbox() {
 	const { userData } = useAuthContext()!;
+	const [searchQuery, setSearchQuery] = useState("");
 
-	return userData?.message !== "user not logged in" ? (
+	console.log(userData);
+
+	const { findUser } = useMessaging();
+
+	return userData?.message !== "user not logged in" && userData ? (
 		<div className="relative flex w-full box-border">
 			<div className="w-full lg:w-1/4 h-screen bg-slate-200 overflow-scroll">
 				<div className="w-full border-2 border-slate-400">
 					<input
 						type="search"
-						placeholder="Enter username"
+						placeholder="Search username to start chatting"
 						className="w-full p-2 outline-none"
+						value={searchQuery}
+						onChange={e => setSearchQuery(e.target.value)}
+						onKeyDown={e => e.key === "Enter" && findUser(searchQuery)}
 					/>
 				</div>
 				<ContactBlock />
-				<div className="bottom-0 p-3 absolute w-72 border-slate-400 border-2 bg-white">
+				<div className="bottom-0 p-3 absolute w-72 border-slate-400 border-2 bg-white flex items-center">
 					<FontAwesomeIcon icon={faBars} className="text-2xl" />
+					<h3 className="text-lg ml-3 font-semibold">
+						Hi, {userData?.username}
+					</h3>
 				</div>
 			</div>
 			<div className="w-full hidden lg:block h-screen">
